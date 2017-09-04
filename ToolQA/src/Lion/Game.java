cf58aeb4ef7100308b45415faad4cd5bde0c087e
Denis Lit <teamovie@mail.ru>
2017-09-04 09:40:29 +0300

    Ащк 10

--- ToolQA/src/Lion/Game.java
+++ ToolQA/src/Lion/Game.java
@@ -20,7 +20,7 @@ public class Game {
 		parts.messageForStart();
 		lion=parts.setStartState(sc);
 		
-		for (int i=0; i<5; i++)
+		for (int i=0; i<10; i++)
 		{
 		
 		parts.messageForInputOutside();
