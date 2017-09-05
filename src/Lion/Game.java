d24136bc7a007c955e2e34717fc82360dd8093ee
Denis Lit <teamovie@mail.ru>
2017-09-05 09:41:29 +0300

    100

--- src/Lion/Game.java
+++ src/Lion/Game.java
@@ -20,7 +20,7 @@ public class Game {
 		parts.messageForStart();
 		lion=parts.setStartState(sc);
 		
-		for (int i=0; i<20; i++)
+		for (int i=0; i<100; i++)
 		{
 		
 		parts.messageForInputOutside();
