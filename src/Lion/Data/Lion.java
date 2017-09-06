5ec5a9b079182a4aec6cebdc50d22bf0abe72fe5
Denis Lit <teamovie@mail.ru>
2017-09-06 11:17:49 +0300

    Add testng

--- src/Lion/Data/Lion.java
+++ src/Lion/Data/Lion.java
@@ -4,11 +4,77 @@ package Lion.Data;
 
 public class Lion {
 	
+	public Lion() {
+		
+		this.state = "";
+		this.statePrevious = "";
+		this.action = "";
+		this.msg = "";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		result = prime * result + ((msg == null) ? 0 : msg.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((statePrevious == null) ? 0 : statePrevious.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lion other = (Lion) obj;
+		if (action == null) {
+			if (other.action != null)
+				return false;
+		} else if (!action.equals(other.action))
+			return false;
+		if (msg == null) {
+			if (other.msg != null)
+				return false;
+		} else if (!msg.equals(other.msg))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (statePrevious == null) {
+			if (other.statePrevious != null)
+				return false;
+		} else if (!statePrevious.equals(other.statePrevious))
+			return false;
+		return true;
+	}
+
 	public String state;
 	public String statePrevious;
-	
 	public String action;
 	public String msg;
+	
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getStatePrevious() {
+		return statePrevious;
+	}
+
+	public void setStatePrevious(String statePrevious) {
+		this.statePrevious = statePrevious;
+	}
 	
 	public String getAction() {
 		return action;
@@ -22,34 +88,24 @@ public class Lion {
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
-	
-		
-	public String getState() {
-		return state;
-	}
-
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-
+			
 	public Lion withState(String state) {
 		this.state = state;
 		return this;
 	}
-
-
-	public String getStatePrevious() {
-		return statePrevious;
-	}
-
-
-	public void setStatePrevious(String statePrevious) {
+	
+	public Lion withStatePrevious(String statePrevious) {
 		this.statePrevious = statePrevious;
+		return this;
 	}
 	
+	public Lion withAction(String action) {
+		this.action= action;
+		return this;
+	}
 	
-	
-
+	public Lion withMsg(String msg) {
+		this.msg=msg;
+		return this;
+	}
 }
---
+++ src/Lion/Test/BehaviorTest.java
@@ -0,0 +1,21 @@
+package Lion.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Lion.Behavior.Behavior;
+import Lion.Data.Lion;
+
+public class BehaviorTest {
+	@Test
+	public void startFromHungry() {
+
+		Behavior b = new 	Behavior ();
+		Lion l = new Lion().withState("h");	
+		Lion etalon = new Lion();
+		
+		etalon.withStatePrevious("h").withState("h").withAction("Run").withMsg("Lion state has not been changed and now is:");
+		Assert.assertEquals(b.behavior("h", l), etalon);
+	
+	}
+}
